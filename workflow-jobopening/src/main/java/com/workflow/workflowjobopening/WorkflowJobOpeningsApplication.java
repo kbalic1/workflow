37b061673be34a81af7c60df9683492db45c1af1
Timur Cerimagic <tcerimagic@gmail.com>
2017-04-08 15:31:42 +0200

    JobOpening hibernate

---
+++ workflow-jobopening/src/main/java/com/workflow/workflowjobopening/WorkflowJobOpeningsApplication.java
@@ -0,0 +1,15 @@
+package com.workflow.workflowjobopening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tile on 4/8/2017.
+ */
+@SpringBootApplication
+public class WorkflowJobOpeningsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowJobOpeningsApplication.class, args);
+    }
+}
--- workflow-jobopening/src/main/java/com/workflow/workflowjobopening/models/JobOffer.java
+++ workflow-jobopening/src/main/java/com/workflow/workflowjobopening/models/JobOffer.java
@@ -1,4 +1,4 @@
-package models;
+package com.workflow.workflowjobopening.models;
 
 import javax.persistence.*;
 import java.util.Date;
