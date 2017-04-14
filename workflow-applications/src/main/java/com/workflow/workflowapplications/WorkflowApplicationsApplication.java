9d6e11903db7811922893762c11035c0439b07c2
Emin Šehić <emin.sehic13@gmail.com>
2017-04-14 16:17:46 +0200

    centralizovana konfiguracija (nije dovršeno)

--- workflow-applications/src/main/java/com/workflow/workflowapplications/WorkflowApplicationsApplication.java
+++ workflow-applications/src/main/java/com/workflow/workflowapplications/WorkflowApplicationsApplication.java
@@ -1,11 +1,29 @@
 package com.workflow.workflowapplications;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WorkflowApplicationsApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplicationsApplication.class, args);
+    }
+}
+
+
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
     }
 }
--- workflow-jobopening/src/main/java/com/workflow/workflowjobopening/WorkflowJobOpeningsApplication.java
+++ workflow-jobopening/src/main/java/com/workflow/workflowjobopening/WorkflowJobOpeningsApplication.java
@@ -1,15 +1,30 @@
+
+
 package com.workflow.workflowjobopening;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by tile on 4/8/2017.
- */
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
-public class WorkflowJobOpeningsApplication {
+public class WorkflowJobOpeningsApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(WorkflowJobOpeningsApplication.class, args);
+    }
+}
+
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
     }
 }
