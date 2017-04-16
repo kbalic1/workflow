90d838709646caa744df8c9f1dcd8e15c6cb9148
Tarik Demirović <tarik.demirovic@atlantbh.com>
2017-04-16 23:37:31 +0200

    Fix error in rest client

--- workflow-applications/src/main/java/com/workflow/workflowapplications/restclients/JobOpeningClient.java
+++ workflow-applications/src/main/java/com/workflow/workflowapplications/restclients/JobOpeningClient.java
@@ -1,6 +1,7 @@
 package com.workflow.workflowapplications.restclients;
 
 import com.workflow.workflowapplications.models.jobopening.JobOffer;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -13,7 +14,7 @@ public class JobOpeningClient {
     }
 
     public JobOffer getJobOpening(Long jobOpeningId) {
-        String uri = "http://jobopening/" + jobOpeningId;
+        String uri = "http://localhost:8014/joboffer/" + jobOpeningId;
         JobOffer jobOffer = restTemplate.getForObject(uri, JobOffer.class);
         return jobOffer;
     }
